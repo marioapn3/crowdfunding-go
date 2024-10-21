359c6bf006d475ce8d018f468bf78d0d5a9b3ea8
marioapn3 <mario.aprilnino27@gmail.com>
2024-10-21 20:08:40 +0700

    feature: api login

--- handler/user.go
+++ handler/user.go
@@ -22,17 +22,9 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	err := c.ShouldBindJSON(&input)
 
 	if err != nil {
-		// var errors []string
-
-		// for _, e := range err.(validator.ValidationErrors) {
-		// 	errors = append(errors, e.Error())
-		// }
 		errors := helper.FormatValidationError(err)
 		errorMessage := gin.H{"errors": errors}
-
 		response := helper.ApiResponse("Register Account Failed", http.StatusUnprocessableEntity, "error", errorMessage)
-
-		// c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
@@ -41,8 +33,6 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	if err != nil {
 		response := helper.ApiResponse("Register Account Failed", http.StatusBadRequest, "error", nil)
-
-		// c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
@@ -52,4 +42,35 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	response := helper.ApiResponse("Account has been registered", http.StatusOK, "success", formatter)
 
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.ApiResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	loggedInUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.ApiResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedInUser, "token")
+
+	response := helper.ApiResponse("Successfuly logged in", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+
 }
--- user/service.go
+++ user/service.go
@@ -1,9 +1,14 @@
 package user
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
+	Login(input LoginInput) (User, error)
 }
 
 type service struct {
@@ -26,7 +31,7 @@ func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
 	if err != nil {
 		return user, err
 	}
-	
+
 	user.PasswordHash = string(passwordHash)
 	user.Role = "user"
 
@@ -39,3 +44,23 @@ func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
 }
 
 // type service struct{}
+func (s *service) Login(input LoginInput) (User, error) {
+	email := input.Email
+	password := input.Password
+
+	user, err := s.repository.FindByEmail(email)
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("No user found on that email")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return user, errors.New("Password incorrect")
+	}
+
+	return user, nil
+}
